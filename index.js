2d5b181a5573bc39106e708a43143b8e855d528e
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-11-07 20:43:25 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hi there</h2>");
+});
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`listing on port :${port}`));
